32ed382e520b62c9ea42f7e536de1d92e59577b9
Vladimir0657305 <vv0657305@gmail.com>
2023-01-11 20:01:15 +0200

    changes

--- src/components/PickAddOns.jsx
+++ src/components/PickAddOns.jsx
@@ -4,17 +4,18 @@ import 'react-input-checkbox/lib/react-input-checkbox.min.css';
 import { Checkbox } from 'react-input-checkbox';
 import './PickAddOns.scss';
 
+const priceAddOns = [1, 2, 2];
 
 export default function PickAddOns() {
-    const {onlineServ, setOnlineServ} = useContext(FormInputData);
-    const {largerStorage, setLargerStorage} = useContext(FormInputData);
-    const {customProfile, setCustomProfile} = useContext(FormInputData);
+    const { onlineServ, setOnlineServ } = useContext(FormInputData);
+    const { largerStorage, setLargerStorage } = useContext(FormInputData);
+    const { customProfile, setCustomProfile } = useContext(FormInputData);
 
     useEffect(() => {
         console.log(onlineServ, largerStorage, customProfile)
     }, [onlineServ, largerStorage, customProfile])
-    
-    
+
+
     return (
         <>
             <section>
@@ -24,17 +25,26 @@ export default function PickAddOns() {
                         <div className="context-main">
                             <div className='context-main__addons'>
                                 <div className='context-addons'  >
-                                    <Checkbox theme="material-checkbox" value={onlineServ} onChange={() => setOnlineServ(!onlineServ)}></Checkbox>
+                                    <Checkbox theme="material-checkbox" value={onlineServ} onChange={() => setOnlineServ(!onlineServ)} className='check'></Checkbox>
+                                    <span className='context-addons__title'>Online service</span>
+                                    <p>Access to multiplayer games</p>
+                                    <span className='context-addons__text'>+${priceAddOns[0]}/mo</span>
                                 </div>
                                 <div className='context-addons'  >
                                     <Checkbox theme="material-checkbox" value={largerStorage} onChange={() => setLargerStorage(!largerStorage)}></Checkbox>
+                                    <span className='context-addons__title'>Larger storage</span>
+                                    <p>Extra 1TB of cloud save</p>
+                                    <span className='context-addons__text'>+${priceAddOns[1]}/mo</span>
                                 </div>
                                 <div className='context-addons'  >
                                     <Checkbox theme="material-checkbox" value={customProfile} onChange={() => setCustomProfile(!customProfile)}></Checkbox>
+                                    <span className='context-addons__title'>Customizable Profile</span>
+                                    <p>Custom theme on your profile</p>
+                                    <span className='context-addons__text'>+${priceAddOns[2]}/mo</span>
                                 </div>
                                 
                             </div>
-                            
+
 
                         </div>
                     </div>
