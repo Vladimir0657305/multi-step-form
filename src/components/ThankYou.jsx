5a4cf026a89704b6f6f0f8924c0b64be3fa103c7
Vladimir0657305 <vv0657305@gmail.com>
2023-01-12 19:37:43 +0200

    changes

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,34 +1,59 @@
-import { useMemo, useContext } from 'react';
+import { useMemo, useContext, useCallback, useEffect, useState } from 'react';
 import { FormInputData } from './Main';
+import { useNavigate } from 'react-router-dom';
 import './ThankYou.scss';
 
 export default function ThankYou() {
     const { stepCount, setStepCount } = useContext(FormInputData);
+    const { onlineServ, setOnlineServ } = useContext(FormInputData);
+    const { largerStorage, setLargerStorage } = useContext(FormInputData);
+    const { customProfile, setCustomProfile } = useContext(FormInputData);
+    const { checkedPlan, setCheckedPlan } = useContext(FormInputData);
+    const { plan, setPlan } = useContext(FormInputData);
+    const { period, setPeriod } = useContext(FormInputData);
+    const [show, setShow] = useState(true);
+    let navigate = useNavigate();
 
-    const displayThankYou = useMemo(
-        () => (
-            
-            <section>
-                <main className='thankYou-main'>
-                    <div className="context-thankYou__main">
-                        <img src='../images/icon-thank-you.svg' alt='ThankYou' />
-                        <h1>Thank You!</h1>
-                        <span>
-                            Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
-                        </span>
-                    </div>
 
-                </main>
-            </section>
-        ),
-        []
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setShow(false);
+            setPlan('Arcade');
+            setPeriod('Monthly');
+            setOnlineServ(false);
+            setLargerStorage(false);
+            setCustomProfile(false);
+            setStepCount(0);
+            setCheckedPlan(false);
+            navigate('/');
+        }, 2000);
+        return () => clearInterval(interval);
+    }, [stepCount === 4]);
+
+
+    const displayThankYou = useCallback(
+
+        <section>
+            <main className='thankYou-main'>
+                <div className="context-thankYou__main">
+                    <img src='../images/icon-thank-you.svg' alt='ThankYou' />
+                    <h1>Thank You!</h1>
+                    <span>
+                        Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.
+                    </span>
+                </div>
+            </main>
+        </section>
+
     )
 
-    return(
+    return (
         <>
             {
-                stepCount === 4 &&  displayThankYou 
+                show && displayThankYou
             }
+
         </>
     );
 }
